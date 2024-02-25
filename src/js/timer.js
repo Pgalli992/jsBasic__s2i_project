d5380a07aff0d22eb9b4a8914ed0986564f1c7ee
Paolo <p.galli@hotmail.it>
2024-02-25 23:27:54 +0100

    Second commit

--- src/js/timer.js
+++ src/js/timer.js
@@ -5,11 +5,10 @@ const days = document.querySelector("#days");
 const hours = document.querySelector("#hours");
 const minutes = document.querySelector("#minutes");
 const seconds = document.querySelector("#seconds");
-const dateSelected = document.querySelector("#date");
+const dateSelected = document.getElementById("date");
 const btnStart = document.querySelector("#btnStart");
 
 // Variables
-const futureDate = dateSelected.value;
 
 // Tabella in ms
 const secondsInMs = 1000;
@@ -18,5 +17,6 @@ const hoursInMs = 60 * minutesInMs;
 const daysInMs = 24 * hoursInMs;
 
 btnStart.addEventListener("click", function () {
+  let futureDate = dateSelected.value;
   console.log(futureDate);
 });
